6680f3c3cbbe2cdad7cfb02a1664a712b7d9cbad
momo <nomofika@gmail.com>
2022-02-03 10:08:38 +0800

    add queryRelation

--- cwn-web-2/src/hooks/useData.js
+++ cwn-web-2/src/hooks/useData.js
@@ -1,13 +1,15 @@
-import React, { useEffect, useState } from "react";
 import Nodes from "../data/cwn_base_nodes.json";
 import Edges from "../data/cwn_base_edges.json";
 
 const useData = () => {
-  const findConnectedNodes = (nodeName) => {
+  const findConnectedNodes = (nodeName, second = true) => {
     return Object.keys(Edges).reduce((result, name) => {
       const splitName = name.split("-");
-      if (splitName[0] === nodeName) {
-        result = [...result, { name: splitName[1], ...Nodes[splitName[1]] }];
+      if (splitName[(second + 1) % 2] === nodeName) {
+        result = [
+          ...result,
+          { name: splitName[+second], ...Nodes[splitName[+second]] },
+        ];
       }
       return result;
     }, []);
@@ -21,8 +23,8 @@ const useData = () => {
       );
     });
     if (!nodeName) return null;
-    const connectedNodes = findConnectedNodes(nodeName);
 
+    const connectedNodes = findConnectedNodes(nodeName);
     return [
       {
         name: Nodes[nodeName].glyph,
@@ -38,14 +40,37 @@ const useData = () => {
   };
 
   const queryLemma = (nodeName) => {
-    const connectedNodes = findConnectedNodes(nodeName, "sense"); // assert node_type === "sense"
+    const connectedNodes = findConnectedNodes(nodeName); // assert node_type === "sense"
     return connectedNodes.map((n) => ({
       ...n,
       // name: n.def,
       name: undefined,
       value: 5,
-      children: [],
+      children: queryRelation(n.name),
     }));
+  };
+
+  const queryRelation = (nodeName) => {
+    console.log(Nodes[nodeName]);
+    const connectedRelationNodes = findConnectedNodes(nodeName);
+    return connectedRelationNodes.map((n) => {
+      const connectedSenseNodes = findConnectedNodes(n.name, false);
+      return connectedSenseNodes.reduce((result, nn) => {
+        const [lemmaNode] = findConnectedNodes(nn.name, false);
+        if (lemmaNode) {
+          return [
+            ...result,
+            {
+              ...lemmaNode,
+              name: Nodes[lemmaNode.name][Nodes[lemmaNode.name].node_type], // lemma || glyph ??
+              value: 100,
+              children: [],
+            },
+          ];
+        }
+        return result;
+      }, [])[0];
+    });
   };
 
   return queryGlyph;
